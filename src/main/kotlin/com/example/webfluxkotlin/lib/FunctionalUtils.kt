6b081189c8235455b04cb218ef09d84eb74b6666
yuji <yuoda@yahoo.co.jp>
2021-11-26 22:31:34 +0900

    brush FunctionalUtils

--- src/main/kotlin/com/example/webfluxkotlin/lib/FunctionalUtils.kt
+++ src/main/kotlin/com/example/webfluxkotlin/lib/FunctionalUtils.kt
@@ -7,14 +7,19 @@ object FunctionalUtils {
 
     fun <T> sequence(stream: Stream<Mono<T>>): Mono<Stream<T>> =
         stream.reduce(Mono.just(Stream.empty()),
-            liftMono { acc, s -> Stream.concat(acc, Stream.of(s)) },
-            liftMono { s1, s2 -> Stream.concat(s1, s2) })
+            liftMono2 { acc, s -> Stream.concat(acc, Stream.of(s)) },
+            liftMono2 { s1, s2 -> Stream.concat(s1, s2) })
 
     fun <T, R> liftMono(f: (T) -> R): (Mono<T>) -> Mono<R> = { it.map(f) }
 
-    fun <T1, T2, R> liftMono(f: (T1, T2) -> R): (Mono<T1>, Mono<T2>) -> Mono<R> =
-        { mt1, mt2 -> mt1.flatMap { t1 -> liftMono { t2: T2 -> f(t1, t2) }(mt2) } }
+    inline fun <T1, T2, R> liftMono2(crossinline f: (T1, T2) -> R): (Mono<T1>, Mono<T2>) -> Mono<R> =
+        { mt1, mt2 -> mt1.flatMap { t1 -> mt2.map { f(t1, it) } } }
 
-    fun <T1, T2, T3, R> liftMono(f: (T1, T2, T3) -> R): (Mono<T1>, Mono<T2>, Mono<T3>) -> Mono<R> =
-        { mt1, mt2, mt3 -> mt1.flatMap { t1 -> liftMono { t2: T2, t3: T3 -> f(t1, t2, t3) }(mt2, mt3) } }
+    inline fun <T1, T2, R> map2(m1: Mono<T1>, m2: Mono<T2>, crossinline f: (T1, T2) -> R): Mono<R> = liftMono2(f)(m1, m2)
+
+    inline fun <T1, T2, T3, R> liftMono3(crossinline f: (T1, T2, T3) -> R): (Mono<T1>, Mono<T2>, Mono<T3>) -> Mono<R> =
+        { mt1, mt2, mt3 -> mt1.flatMap { t1 -> map2(mt2, mt3) { t2, t3 -> f(t1, t2, t3) } } }
+
+    inline fun <T1, T2, T3, R> map3(m1: Mono<T1>, m2: Mono<T2>, m3: Mono<T3>, crossinline f: (T1, T2, T3) -> R): Mono<R> =
+        liftMono3(f)(m1, m2, m3)
 }
