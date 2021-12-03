376ddbf1c210a67680a4cc9f7eb72f2e3bf2f745
yuji <yuoda@yahoo.co.jp>
2021-12-03 10:14:36 +0900

    add FunctionalUtils

--- src/main/kotlin/com/example/webfluxkotlin/lib/FunctionalUtils.kt
+++ src/main/kotlin/com/example/webfluxkotlin/lib/FunctionalUtils.kt
@@ -8,17 +8,18 @@ object FunctionalUtils {
     fun <T> sequence(stream: Stream<Mono<T>>): Mono<Stream<T>> =
         stream.reduce(Mono.just(Stream.empty()), liftMono2(::cons), liftMono2 { s1, s2 -> Stream.concat(s1, s2) })
 
-    fun <T, R> liftMono(f: (T) -> R): (Mono<out T>) -> Mono<R> = { it.map(f) }
+    fun <T, R> liftMono(f: (T) -> R): (Mono<T>) -> Mono<R> = { it.map(f) }
 
-    inline fun <T1, T2, R> liftMono2(crossinline f: (T1, T2) -> R): (Mono<out T1>, Mono<out T2>) -> Mono<R> =
+    inline fun <T1, T2, R> liftMono2(crossinline f: (T1, T2) -> R): (Mono<T1>, Mono<T2>) -> Mono<R> =
         { mt1, mt2 -> mt1.flatMap { t1 -> mt2.map { f(t1, it) } } }
 
-    inline fun <T1, T2, R> map2(m1: Mono<out T1>, m2: Mono<out T2>, crossinline f: (T1, T2) -> R): Mono<R> = liftMono2(f)(m1, m2)
+    inline fun <T1, T2, R> map2(m1: Mono<T1>, m2: Mono<T2>, crossinline f: (T1, T2) -> R): Mono<R> =
+        liftMono2(f)(m1, m2)
 
-    inline fun <T1, T2, T3, R> liftMono3(crossinline f: (T1, T2, T3) -> R): (Mono<out T1>, Mono<out T2>, Mono<out T3>) -> Mono<R> =
+    inline fun <T1, T2, T3, R> liftMono3(crossinline f: (T1, T2, T3) -> R): (Mono<T1>, Mono<T2>, Mono<T3>) -> Mono<R> =
         { mt1, mt2, mt3 -> mt1.flatMap { t1 -> map2(mt2, mt3) { t2, t3 -> f(t1, t2, t3) } } }
 
-    inline fun <T1, T2, T3, R> map3(m1: Mono<out T1>, m2: Mono<out T2>, m3: Mono<out T3>, crossinline f: (T1, T2, T3) -> R): Mono<R> =
+    inline fun <T1, T2, T3, R> map3(m1: Mono<T1>, m2: Mono<T2>, m3: Mono<T3>, crossinline f: (T1, T2, T3) -> R): Mono<R> =
         liftMono3(f)(m1, m2, m3)
 
     fun <T> cons(stream: Stream<out T>, t: T): Stream<T> = Stream.concat(stream, Stream.of(t))
